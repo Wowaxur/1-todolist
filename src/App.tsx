a04e364a5ddfc1bb11df9b7a3afc2de67cd64144
Wowaxur <wowaxur@yandex.ru>
2023-12-04 19:33:24 +0300

    add new id to tasks start creating function removeTask

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,43 @@ import {TodoList} from "./components/todolist/TodoList";
 
 
 function App() {
-    const tasks1 = [
+    const tasks = [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
+        { id: 3, title: "ReactJS", isDone: false },
+        { id: 4, title: "Redux", isDone: false },
     ]
+    function
+    removeTask(id: number) {
+        let resultTasks = tasks.filter(() => {
+            return true
+        })
+        console.log(resultTasks)
+    }
     const tasks2 = [
         { id: 1, title: "Hello world", isDone: true },
         { id: 2, title: "I am Happy", isDone: false },
-        { id: 3, title: "Yo", isDone: false }
+        { id: 3, title: "Open Window", isDone: false },
+/*
+        { id: 4, title: "Yo", isDone: false },
+*/
     ]
-    return (
+
+
+return (
         <div className="App">
-          <TodoList title = 'What to learn' tasks = {tasks1}/>
+          <TodoList
+              title = 'What to learn'
+              tasks = {tasks}
+              removeTask={removeTask}
+          /><TodoList
+              title = 'Songs'
+              tasks = {tasks2}
+              removeTask={removeTask}
+          />
+{/*
           <TodoList title = 'Songs'  tasks = {tasks2}/>
+*/}
 
         </div>
     );
